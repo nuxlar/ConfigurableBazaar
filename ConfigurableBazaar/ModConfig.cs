6b5c5abc75a04f9274648c816ef54ed5ac311306
Romy Maghsoudi <romy.mag9@gmail.com>
2022-06-25 07:56:47 -0400

    v1.0.1 release

--- ConfigurableBazaar/ModConfig.cs
+++ ConfigurableBazaar/ModConfig.cs
@@ -1,18 +1,27 @@
 ﻿using BepInEx.Configuration;
-using System.Collections.Generic;
 using System;
 
 namespace ConfigurableBazaar
 {
     internal class ModConfig
     {
-        public static ConfigEntry<int> lunarItemLimit;
         public static ConfigEntry<int> newtPortal;
+
+        public static ConfigEntry<int> lunarItemLimit;
+        public static ConfigEntry<int> lunarItemPrice;
+        public static ConfigEntry<int> lunarSeerPrice;
+        public static ConfigEntry<int> lunarRollPrice;
+        public static ConfigEntry<int> lunarRollAmount;
+        public static ConfigEntry<bool> respawnLunars;
+
         public static ConfigEntry<bool> spawnScrapper;
-        public static ConfigEntry<bool> spawnCauldron;
+        public static ConfigEntry<bool> spawnR2WCauldron;
+        public static ConfigEntry<bool> spawnG2WCauldron;
         public static ConfigEntry<bool> spawnCleansingPool;
-        public static ConfigEntry<bool> greenToWhite;
-        public static ConfigEntry<int> itemsToReforge;
+        public static ConfigEntry<bool> redScrapCauldron;
+        public static ConfigEntry<int> itemsToReforgeG;
+        public static ConfigEntry<int> itemsToReforgeR;
+
         public static ConfigEntry<int> printerCount;
         public static ConfigEntry<float> tier1Chance;
         public static ConfigEntry<float> tier2Chance;
@@ -21,17 +30,24 @@ namespace ConfigurableBazaar
 
         public static void InitConfig(ConfigFile config)
         {
-            lunarItemLimit = config.Bind("General", "Lunar Item Limit", 5, new ConfigDescription("**DOESNT WORK** Set how many lunar items each person can take from the bazaar"));
             newtPortal = config.Bind("General", "Newt Portal", 0, new ConfigDescription("Set which stages the blue portal should spawn (0: Vanilla) (12345: Every Stage)"));
             spawnScrapper = config.Bind("General", "Enable Scrapper", true, new ConfigDescription("Set if scrapper should spawn in the bazaar"));
             spawnCleansingPool = config.Bind("General", "Enable Cleansing Pool", false, new ConfigDescription("Set if cleansing pool should spawn in the bazaar"));
-            spawnCauldron = config.Bind("Lunar Cauldron", "Enable Reforge Cauldron", true, new ConfigDescription("Set if cauldron should spawn in the bazaar"));
-            greenToWhite = config.Bind("Lunar Cauldron", "Green To White", true, new ConfigDescription("Set if cauldron should reforge greens (instead of a red) to 3 whites"));
-            itemsToReforge = config.Bind("Lunar Cauldron", "Items To Reforge", 2, new ConfigDescription("Set how many items should be reforged (Defaults - green: 2, red: 1)"));
+
+            lunarItemLimit = config.Bind("Lunar Shop", "Lunar Item Limit", 5, new ConfigDescription("Set how many lunar items each person can take from the bazaar"));
+            lunarItemPrice = config.Bind("Lunar Shop", "Lunar Item Price", 2, new ConfigDescription("Set how much lunar items cost"));
+            lunarSeerPrice = config.Bind("Lunar Shop", "Lunar Seer Price", 2, new ConfigDescription("Set how much the lunar seers cost"));
+            lunarRollPrice = config.Bind("Lunar Shop", "Lunar Roll Price", -1, new ConfigDescription("Set how much the rolls should cost (-1: Vanilla)"));
+            lunarRollAmount = config.Bind("Lunar Shop", "Lunar Roll Amount", -1, new ConfigDescription("Set how many times you can roll the lunar shop (-1: Vanilla)"));
+            respawnLunars = config.Bind("Lunar Shop", "Respawn Lunars", false, new ConfigDescription("Set if lunar items should respawn after every roll"));
+
+            redScrapCauldron = config.Bind("Lunar Cauldrons", "Enable Red Scrap Cauldron", true, new ConfigDescription("Set if red cauldrons should take red scrap"));
+            spawnR2WCauldron = config.Bind("Lunar Cauldrons", "Enable Red to White Cauldron", false, new ConfigDescription("Set if Red to White Cauldron should spawn in the bazaar"));
+            spawnG2WCauldron = config.Bind("Lunar Cauldrons", "Enable Green to White Cauldron", true, new ConfigDescription("Set if Green to White Cauldron should spawn in the bazaar"));
+            itemsToReforgeG = config.Bind("Lunar Cauldrons", "Items To Reforge for Red Cauldron", 2, new ConfigDescription("Set how many items should be reforged for the Green to White cauldron"));
+            itemsToReforgeR = config.Bind("Lunar Cauldrons", "Items To Reforge for Green Cauldron", 1, new ConfigDescription("Set how many items should be reforged for the Red to White cauldron"));
+
             printerCount = config.Bind("Printers", "Printer Count", 1, new ConfigDescription("Set how many 3D Printers should spawn in the bazaar. Maximum is 4"));
-            printerCount.Value = Math.Abs(printerCount.Value);
-            if (printerCount.Value > 4)
-                printerCount.Value = 4;
             tier1Chance = config.Bind("Printers", "Tier 1 Chance", 0.7f, new ConfigDescription("Set how likely it is for a bazaar 3D Printer to be tier 1"));
             tier1Chance.Value = Math.Abs(tier1Chance.Value);
             tier2Chance = config.Bind("Printers", "Tier 2 Chance", 0.2f, new ConfigDescription("Set how likely it is for a bazaar 3D Printer to be tier 2"));
--- README.md
+++ README.md
@@ -1,47 +1,84 @@
 # ConfigurableBazaar
-**WORK IN PROGRESS**
 
-Working to make the bazaar completely configurable through rolls, interactables, prices, and portal spawns. Bazaar kickout is now removed.
+Server-side/Vanilla compatible.
+This mod aims to make the bazaar completely configurable by adding interactables, limiting lunar rolls/pickups, adding extra functionality, and preventing kickout for the party. Bazaar kickout prevention works for everyone even on vanilla clients. Also if only the host has the mod the green cauldron color will look red and scrap text won't show for vanilla players.
 Currently has configuration for:
 
-	- portal spawns!
-	- a scrapper
-	- printers (up to 4)
-	- a cleansing pool
-	- an extra cauldron (green to white or red to white)
+    - lunar prices
+    - lunar rolls
+    - lunar items per player
+    - lunar items respawning after rolls
+    - blue portal spawns
+    - a scrapper
+    - a cleansing pool
+    - extra cauldrons (red/green to white)
+    - printers (up to 4)
+    - red cauldrons taking red scrap
 
 ## Config Info
+
 **Portal Config:**
-* 0 for no change (default), 12345 for a portal every stage
-* the numbers apply to both pre and post loop
-* the stage numbers that aren't in WONT spawn a portal even if you hit a newt shrine e.g. (135 wont spawn portals on stages 4 and 5)
+
+- 0 for no change (default), 12345 for a portal every stage
+- the numbers apply to both pre and post loop
+- the stage numbers that aren't in WONT spawn a portal even if you hit a newt shrine e.g. (135 wont spawn portals on stages 2 and 4)
 
 ## Contact
-Reach out with bugs/feedback **leon_#0235**
+
+Reach out with bugs/feedback **nuxlar#0235**
+
+## Donate
+
+If you like what I'm doing contribute to my caffeine addiction! https://ko-fi.com/nuxlar
 
 ## Changelog
+
+**1.0.1**
+
+- README update
+
+**1.0.0**
+
+- DELETE YOUR CONFIGURABLEBAZAAR CONFIG FILE IF YOURE UPDATING
+- Adds config for bazaar prices (pods, seers, rolls)
+- Adds config for using red scrap for red cauldrons
+- Adds config for lunar items per player
+- Adds config for rolling the lunar shop
+- Adds config for lunar items respawning after each roll
+- Adds broadcast message with rules/config
+- Separates red to white and green to white cauldrons
+- Fixes only host getting kickout exemption
+
 **0.6.5**
-* Removes bazaar kickout
+
+- Removes bazaar kickout
 
 **0.6.0**
-* Added config for newt portal spawns
-* Fixed the hologram text/color for realsies this time (i hope)
+
+- Added config for newt portal spawns
+- Fixed the hologram text/color for realsies this time (i hope)
 
 **0.5.4**
-* Adds hologram text on cauldron that explicitly says the exchange rates
+
+- Adds hologram text on cauldron that explicitly says the exchange rates
 
 **0.5.3**
-* Fixes hologram text on cauldron
+
+- Fixes hologram text on cauldron
 
 **0.5.2**
-* Fixes cauldron not spawning in multiplayer
-* README edit.
+
+- Fixes cauldron not spawning in multiplayer
+- README edit.
 
 **0.5.1**
-* Readme edit.
+
+- Readme edit.
 
 **0.5.0**
-* Adds config for cleansing pool and extra cauldron (red/green to white).
+
+- Adds config for cleansing pool and extra cauldron (red/green to white).
 
 **0.3.0**
-* Adds config for scrapper and printers (up to 4).
+
+- Adds config for scrapper and printers (up to 4).
